0901b518cc2fe99ddba3f73677897ba72faa0576
Martin Pham <martin.liem.pham@gmail.com>
2018-06-12 17:12:06 -0400

    added header

--- app/style.js
+++ app/style.js
@@ -5,4 +5,24 @@ const constants = {
   actionColor: '#3cb371'
 };
 
-module.exports = StyleSheet.create({});
+module.exports = StyleSheet.create({
+  navbar: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderBottomColor: '#eee',
+    borderColor: 'transparent',
+    borderWidth: 1,
+    justifyContent: 'center',
+    height: 44,
+    flexDirection: 'row'
+  },
+  navbarTitle: {
+    color: '#444',
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  toolbar: {
+    backgroundColor: '#fff',
+    height: 22
+  }
+});
